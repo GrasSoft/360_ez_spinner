77c8810a652ba516e0e58d40591d75c70f52e089
Cristian <cristian.cutitei27@yahoo.com>
2024-07-11 17:25:50 +0200

    Refactored code so it can be easily moved to other files thus reducing clutter. Additional spin object instead of camera featura

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@
 
 import bpy
 from mathutils import Vector
-from math import pi
+from math import pi, radians
 import math
 
 bl_info = {
@@ -27,7 +27,12 @@ bl_info = {
     "category" : "Generic"
 }
 
-camera_object = None
+camera_object_name = "360 Spinner Camera"
+curve_object_name = "Camera Track Curve"
+
+# Function to check if the object is valid
+def is_object_valid(object_name):
+    return object_name in bpy.data.objects
 
 class VIEW3D_PT_main_panel(bpy.types.Panel):
     bl_label = "360 Spinner"
@@ -40,8 +45,8 @@ class VIEW3D_PT_main_panel(bpy.types.Panel):
         layout = self.layout
         layout.label(text="Testicular Torsion!")
 
-        print(camera_object)
-        if camera_object:
+        if is_object_valid(camera_object_name):
+            camera_object = bpy.data.objects[camera_object_name]
             col = layout.column()
             col.prop(camera_object, "location", text="Location")
             col = layout.column()
@@ -50,9 +55,35 @@ class VIEW3D_PT_main_panel(bpy.types.Panel):
         layout.separator()
         row = layout.row()
         row.operator("object.add_360_spinner_camera",
-                        text="Add 360 Spinner Camera")    
+                        text="Add 360 Spinner Camera")
+        row = layout.row()
+        row.operator("object.spin_object",
+                        text="Spin selected object")    
 
+class OBJECT_OT_spin_object(bpy.types.Operator):
+    bl_idname = "object.spin_object"
+    bl_label = "Spin object"
+    bl_description = "Spin the object around by 360 degrees"
 
+    def add_keyframes(self, obj, num_frames):
+        # Convert rotation amount to radians
+        rotation_amount_radians = radians(360)
+
+        # Create keyframes
+        for frame in range(num_frames + 1):
+            bpy.context.scene.frame_set(frame)
+            obj.rotation_euler[2] = rotation_amount_radians * frame / num_frames
+            obj.keyframe_insert(data_path="rotation_euler", index=2)
+
+        # Set the scene's end frame
+        bpy.context.scene.frame_end = num_frames
+
+    def execute(self, context):
+        obj = context.object
+
+        self.add_keyframes(obj, 100)
+
+        return {'FINISHED'}
 
 
 class OBJECT_OT_add_360_spinner_camera(bpy.types.Operator):
@@ -99,14 +130,19 @@ class OBJECT_OT_add_360_spinner_camera(bpy.types.Operator):
         # Set the origin of the curve
         curve_object.location = origin
 
+        curve_object.name = curve_object_name
+        curve_object.data.name = curve_object_name
+
         # Close the spline to make it cyclic
         spline.use_cyclic_u = True
 
         return curve_object
 
-    def set_camera_track(self, camera, target):
+    def set_camera_track(self, target):
+
+        camera_object = bpy.data.objects[camera_object_name]
         # Add a 'Track To' constraint to the camera
-        track_to = camera.constraints.new(type='TRACK_TO')
+        track_to = camera_object.constraints.new(type='TRACK_TO')
 
         # Set the target of the constraint
         track_to.target = target
@@ -115,9 +151,13 @@ class OBJECT_OT_add_360_spinner_camera(bpy.types.Operator):
         track_to.track_axis = 'TRACK_NEGATIVE_Z'    # Camera looks along the -Z axis
         track_to.up_axis = 'UP_Y'                   # Y axis is considered up
 
-    def set_camera_follow(self, camera, curve):
+    def set_camera_follow(self):
+     
+        camera_object = bpy.data.objects[camera_object_name]
+        curve = bpy.data.objects[curve_object_name]
+
         # Add Follow Path constraint to the camera
-        follow_path_constraint = camera.constraints.new(type='FOLLOW_PATH')
+        follow_path_constraint = camera_object.constraints.new(type='FOLLOW_PATH')
         follow_path_constraint.target = curve  # Set the curve as the target for the constraint
         follow_path_constraint.use_fixed_location = False  # Ensure the camera stays on the curve
 
@@ -131,53 +171,67 @@ class OBJECT_OT_add_360_spinner_camera(bpy.types.Operator):
         follow_path_constraint.offset = 100  # End position at the end of the curve (adjust as needed)
         follow_path_constraint.keyframe_insert(data_path='offset', frame=250)  # Insert keyframe at frame 250
 
+    def get_camera_information(self):
+        camera_object = bpy.data.objects[camera_object_name]
+
+        render_settings = bpy.context.scene.render
+
+        # this is the aspect ratio of the redner resolution divided by the aspect ratio of the sensor, used to scale the height of the image        
+        aspect_ratio_correction = (render_settings.resolution_x / render_settings.resolution_y) / (camera_object.data.sensor_width/camera_object.data.sensor_height)
+        
+        # half of the sensor width, scaled to mm
+        sensor_width_half = camera_object.data.sensor_width/2/1000
+ 
+        # half of the sensor height, scaled to mm
+        sensor_height_half = camera_object.data.sensor_height/2/1000
+
+        return (aspect_ratio_correction, sensor_width_half, sensor_height_half)
+
+    def get_track_radius(self, obj):
+        camera_object = bpy.data.objects[camera_object_name]
+
+
+        (aspect_ratio_correction, sensor_width_half, sensor_height_half) = self.get_camera_information()
+
+        dimension = max(obj.dimensions)/2
+        # This only applies in the case wehre the aspect ratio of the camera makes it so the height is lower than the, width (TODO check different cases)
+        # Increase the margins of the object by increasing the dimensions of the object with a certain amount
+        dimension += 0.5
+        distance_height = dimension/(sensor_height_half) * (camera_object.data.lens/1000) * aspect_ratio_correction
+        distance_width =  dimension/(sensor_width_half)  * (camera_object.data.lens/1000)
+
+        # take the maximum distance out out of the 2 so we make sure every part of the object is in the frame
+        return max(distance_height, distance_width)
+
+    def create_camera(self):
+        bpy.ops.object.camera_add()
+        camera_object = bpy.context.active_object
+
+        camera_object.name = camera_object_name
+        camera_object.data.name = camera_object_name
+
 
 
     def execute(self, context):
         # get selected object 
         obj = context.object
         
-
         global camera_object
-        # Create a new camera object
+        self.create_camera()
 
-
-        bpy.ops.object.camera_add(rotation=(pi/2,0,pi/2))
-        camera_object = bpy.context.active_object
-
-        dimension = max(obj.dimensions)/2
-        render_settings = bpy.context.scene.render
-        aspect_ratio = (render_settings.resolution_x / render_settings.resolution_y) / (camera_object.data.sensor_width/camera_object.data.sensor_height)
-        sensor_width_half = camera_object.data.sensor_width/2/1000
-        sensor_height_half = camera_object.data.sensor_height/2/1000
- 
-        # This only applies in the case wehre the aspect ratio of the camera makes it so the height is lower than the, width (TODO check different cases)
-        # Increase the margins of the object by increasing the dimensions of the object with a certain amount
-        dimension += 0.5
-        distance_height = dimension/(sensor_height_half) * (camera_object.data.lens/1000) * aspect_ratio 
-        distance_width =  dimension/(sensor_width_half)  * (camera_object.data.lens/1000)
-
-        distance = max(distance_height, distance_width)
-
-
-        camera_object.location = obj.location + Vector((distance + dimension, 0, 0))
-
-
-        curve_object = self.create_bezier_circle(Vector((obj.location - camera_object.location)).length, obj.location)
-        camera_object.location = obj.location
-
-        self.set_camera_follow(camera_object, curve_object)
+   
+        radius = self.get_track_radius(obj)
+        self.create_bezier_circle(radius, obj.location)
         
-        self.set_camera_track(camera_object, obj)
-
-
         
-        camera_object.name = "360 Spinner Camera"
-        camera_object.data.name = "360 Spinner Camera"
+        # order matters
+        self.set_camera_follow()
+        
+        self.set_camera_track(obj)
 
         return {'FINISHED'}
 
-class_list = [VIEW3D_PT_main_panel, OBJECT_OT_add_360_spinner_camera]
+class_list = [VIEW3D_PT_main_panel, OBJECT_OT_add_360_spinner_camera, OBJECT_OT_spin_object]
 
 def register():
     for cls in class_list:
