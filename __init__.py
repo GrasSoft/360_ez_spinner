70dae4f19789155628427b44db3b51136ffef892
Cristian <cristian.cutitei27@yahoo.com>
2024-07-14 17:13:08 +0200

    UI changes

--- __init__.py
+++ __init__.py
@@ -28,37 +28,89 @@ bl_info = {
     "category" : "Generic"
 }
 
+class MyProperties(bpy.types.PropertyGroup):
+    object_up_axis: bpy.props.EnumProperty(
+        name= "Object UP axis",
+        description= "In case you object is not alligned with the camera, change the up axis.", 
+        items= [('X', "X", "The up axis will become X."),
+                ('Y', "Y", "The up axis will become Y."),        
+                ('Z', "Z", "The up axis will become Z.")
+        ]
+    )
+
+    movement_type : bpy.props.EnumProperty(
+        name= "Movement type",
+        description= "Select wether the objects or the camera spins.",
+        items= [('OP1', "Object rotates", "The camera stays in place and object rotates",'WORLD_DATA', 0),
+                ('OP2', "Camera rotates", "The object stays in place and camera rotates",'WORLD_DATA', 1),
+               
+        ]
+    )
+
 class VIEW3D_PT_main_panel(bpy.types.Panel):
-    bl_label = "360 Spinner"
+    bl_label = "SpinWiz"
     bl_idname = "VIEW3D_PT_main_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = '360 Spinner'
+    bl_category = 'SpinWiz'
 
     def draw(self, context):
+        scene = context.scene
+        spin_settings = scene.spin_settings
+
         layout = self.layout
-        layout.label(text="Testicular Torsion!")
+        layout.scale_y = 1.5
+        
 
-        # if is_object_valid(camera_object_name):
-        #     camera_object = bpy.data.objects[camera_object_name]
-        #     col = layout.column()
-        #     col.prop(camera_object, "location", text="Location")
-        #     col = layout.column()
-        #     col.prop(camera_object, "rotation_euler", text="Rotation")
+        if len(bpy.context.selected_objects) == 0:
+            row = layout.row(align=True)
+            row.alignment = "CENTER"
+            row.label(text="Please select a suitable object")        
+        else:
+            # if is_object_valid(camera_object_name):
+            #     camera_object = bpy.data.objects[camera_object_name]
+            #     col = layout.column()
+            #     col.prop(camera_object, "location", text="Location")
+            #     col = layout.column()
+            #     col.prop(camera_object, "rotation_euler", text="Rotation")
 
-        if is_object_valid(curve_object_name):
-            camera_object = bpy.data.objects[curve_object_name]
-            col = layout.column()
-            col.prop(camera_object, "location", text="Location")
-            col = layout.column()
-            col.prop(camera_object, "rotation_euler", text="Rotation")
-        layout.separator()
+            layout.separator()
+            row = layout.row()
+            column = row.column()
+            column.operator("object.documentation", text="Motion Setup")
+            column = row.column()
+            column.operator("object.documentation", text="Output")
+            layout.separator()
+
+            box = layout.box()
+            box.prop(spin_settings, "movement_type")
+
+            row = box.row(align=True)
+            row.label(text="Interpolation")
+            row.prop(spin_settings, "movement_type", expand=True, icon_only=True, icon="BLENDER")
+
+            
+
+
+            row = layout.row()
+            row.operator("object.spin_object",
+                            text="Spin selected object")    
+            
+            layout.separator()
+        # documentation button
         row = layout.row()
-        row.operator("object.add_360_spinner_camera",
-                        text="Add 360 Spinner Camera")
-        row = layout.row()
-        row.operator("object.spin_object",
-                        text="Spin selected object")    
+        row.operator("object.documentation",
+                        text="Documentation")    
+
+
+class OBJECT_OT_documentation(bpy.types.Operator):
+    bl_idname = "object.documentation"
+    bl_label = "Documentation"
+    bl_description = "Go to documentation"
+
+    def execute(self, context):
+        return {"FINISHED"}
+
 
 class OBJECT_OT_spin_object(bpy.types.Operator):
     bl_idname = "object.spin_object"
@@ -120,17 +172,18 @@ class OBJECT_OT_add_360_spinner_camera(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class_list = [VIEW3D_PT_main_panel, OBJECT_OT_add_360_spinner_camera, OBJECT_OT_spin_object]
+class_list = [MyProperties, VIEW3D_PT_main_panel, OBJECT_OT_add_360_spinner_camera, OBJECT_OT_spin_object, OBJECT_OT_documentation]
 
 def register():
     for cls in class_list:
         bpy.utils.register_class(cls)
 
+        bpy.types.Scene.spin_settings = bpy.props.PointerProperty(type= MyProperties)
+
     
 def unregister():
     for cls in class_list:
         bpy.utils.unregister_class(cls)
-
-
+        #del bpy.types.Scene.spin_settings
 if __name__ == "__main__":
     register()
